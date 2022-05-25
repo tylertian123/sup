56448c37c7f4e1e3d855e999c3586ec6040e2203
Tyler Tian <tylertian123@gmail.com>
2022-05-25 04:11:27 -0400

    Add clear button

--- server/pages/index.jsx
+++ server/pages/index.jsx
@@ -6,6 +6,9 @@ import { Alert, Button, Container, Form } from 'react-bootstrap';
 import Layout from '../components/Layout';
 import MultiDisplay, { createDefaultValues, packValues, unpackValues } from '../components/MultiDisplay'
 
+const DISPLAY_WIDTH = 4;
+const DISPLAY_HEIGHT = 2;
+
 const db = firebase.database();
 try {
     db.useEmulator('localhost', 9000);
@@ -36,7 +39,7 @@ export default function Home() {
     const [loginUser, setLoginUser] = useState(null);
     const [configOk, setConfigOk] = useState(true);
     const [success, setSuccess] = useState(false);
-    const [displayValues, setDisplayValues] = useState(createDefaultValues(2, 2));
+    const [displayValues, setDisplayValues] = useState(createDefaultValues(DISPLAY_HEIGHT, DISPLAY_WIDTH));
     const writeTo = useRef(null);
 
     // Make sure the page is updated when auth state is changed, since the auth user starts out null
@@ -60,7 +63,7 @@ export default function Home() {
                             alert("Error: Data is not in the expected format!");
                         }
                         else {
-                            setDisplayValues(deserializeDisplay(snapshot.val().slice("blob,base64,".length), 8 * 2));
+                            setDisplayValues(deserializeDisplay(snapshot.val().slice("blob,base64,".length), 8 * DISPLAY_WIDTH));
                         }
                     }
                 });
@@ -105,6 +108,7 @@ export default function Home() {
                         <MultiDisplay values={displayValues} setValues={setDisplayValues}></MultiDisplay>
                     </Form.Group>
                     <Button type="submit" disabled={!configOk}>Submit</Button>
+                    <Button className="mx-2" variant="danger" disabled={!configOk} onClick={() => setDisplayValues(createDefaultValues(DISPLAY_HEIGHT, DISPLAY_WIDTH))}>Clear</Button>
                 </Form>
             </Container>
         </Layout>
