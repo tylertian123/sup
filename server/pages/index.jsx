e1b96b894781fe4ba6ff287ca1ca1eb33dc59701
Tyler Tian <tylertian123@gmail.com>
2022-05-24 21:47:14 -0400

    Add alert on homepage for config

--- server/pages/index.jsx
+++ server/pages/index.jsx
@@ -1,13 +1,16 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
-import { useEffect, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+import { Alert, Button, Container, Form } from 'react-bootstrap';
 import Layout from '../components/layout';
 
 const db = firebase.database();
 
 export default function Home() {
     const [loginUser, setLoginUser] = useState(null);
+    const [configOk, setConfigOk] = useState(true);
+    const writeTo = useRef(null);
 
     // Make sure the page is updated when auth state is changed, since the auth user starts out null
     useEffect(() =>
@@ -15,6 +18,18 @@ export default function Home() {
             setLoginUser(user);
         }), []
     );
+
+    // When user is loaded, try to find where to write the data and show the alert if it's not configured
+    if (db && loginUser && writeTo.current === null && configOk) {
+        db.ref(`/users/${loginUser.uid}/writeTo`).get().then((snapshot) => {
+            if (!snapshot.exists()) {
+                setConfigOk(false);
+            }
+            else {
+                writeTo.current = snapshot.val();
+            }
+        });
+    }
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
@@ -25,21 +40,20 @@ export default function Home() {
             alert("Error: Not logged in!");
             return;
         }
-        db.ref("/users/" + loginUser.uid + "/writeTo").get().then((snapshot) => {
-            if (!snapshot.exists()) {
-                alert("Error: Don't know where to write to!");
-                return;
-            }
-            const data = new FormData(form);
-            db.ref("/data/" + snapshot.val()).set({
-                testData: data.get("testData")
-            });
+        if (writeTo.current === null) {
+            alert("Error: Don't know where to write to!");
+            return;
+        }
+        const data = new FormData(form);
+        db.ref("/data/" + writeTo.current).set({
+            testData: data.get("testData")
         });
     };
 
     return (
         <Layout title="Home" navbarActiveKey="home">
             <Container>
+                {!configOk ? <Alert variant="warning">You need to <Link href="/config">configure some settings</Link> before you can use this app.</Alert> : null}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Send Test Data</Form.Label>
