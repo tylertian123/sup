15d2fdb2b760987d368d20cc3f70a1b57c716a99
Tyler Tian <tylertian123@gmail.com>
2022-05-26 03:18:23 -0400

    Add mouse drag to draw on display

--- server/components/Display.jsx
+++ server/components/Display.jsx
@@ -1,16 +1,41 @@
 import styles from './Display.module.css'
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Display({ values, setValues, style = {} }) {
     const cols = values[0].length;
+    const mouseDown = useRef(false);
+
+    function mouseDownListener() {
+        mouseDown.current = true;
+    }
+    function mouseUpListener() {
+        mouseDown.current = false;
+    }
+    useEffect(() => {
+        document.addEventListener("mousedown", mouseDownListener);
+        document.addEventListener("mouseup", mouseUpListener);
+        return () => {
+            document.removeEventListener("mousedown", mouseDownListener);
+            document.removeEventListener("mouseup", mouseUpListener);
+        };
+    });
+
+    function onChange(i, j, event) {
+        const copy = values.map((e) => [...e]);
+        copy[i][j] = event.currentTarget.checked;
+        setValues(copy);
+    }
     const leds = values.map(
         (row, i) => row.map(
             (state, j) =>
-                <input key={[i, j]} type="checkbox" className={styles.led} checked={state} onChange={(event) => {
-                    const copy = values.map((e) => [...e]);
-                    copy[i][j] = event.currentTarget.checked;
-                    setValues(copy);
-                }} />
+                <input key={[i, j]} type="checkbox" className={styles.led} checked={state} 
+                    onChange={onChange.bind(null, i, j)}
+                    onMouseOver={(event) => {
+                        if (mouseDown.current) {
+                            event.currentTarget.checked = !event.currentTarget.checked;
+                            onChange(i, j, event);
+                        }
+                    }} />
         )
     );
 
