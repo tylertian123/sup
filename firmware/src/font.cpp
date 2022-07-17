c508ef748bf2510aabf70e3259bc2c93c0e73c60
Tyler Tian <tylertian123@gmail.com>
2022-07-17 15:59:12 -0400

    Add numbers and percent sign to font

--- firmware/src/font.cpp
+++ firmware/src/font.cpp
@@ -29,6 +29,14 @@ namespace graphics {
         _PAD(0b1),
     };
 
+    const PROGMEM uint8_t CHAR_PCT_DATA[] = {
+        _PAD(0b11001),
+        _PAD(0b11010),
+        _PAD(0b00100),
+        _PAD(0b01011),
+        _PAD(0b10011),
+    };
+
     const PROGMEM uint8_t CHAR_A_DATA[] = {
         _PAD(0b0110),
         _PAD(0b1001),
@@ -237,9 +245,90 @@ namespace graphics {
         _PAD(0b1111),
     };
 
+    const PROGMEM uint8_t CHAR_0_DATA[] = {
+        _PAD(0b0110),
+        _PAD(0b1001),
+        _PAD(0b1001),
+        _PAD(0b1001),
+        _PAD(0b0110),
+    };
+
+    const PROGMEM uint8_t CHAR_1_DATA[] = {
+        _PAD(0b010),
+        _PAD(0b110),
+        _PAD(0b010),
+        _PAD(0b010),
+        _PAD(0b111),
+    };
+
+    const PROGMEM uint8_t CHAR_2_DATA[] = {
+        _PAD(0b0110),
+        _PAD(0b1001),
+        _PAD(0b0010),
+        _PAD(0b0100),
+        _PAD(0b1111),
+    };
+
+    const PROGMEM uint8_t CHAR_3_DATA[] = {
+        _PAD(0b1110),
+        _PAD(0b0001),
+        _PAD(0b0110),
+        _PAD(0b0001),
+        _PAD(0b1110),
+    };
+
+    const PROGMEM uint8_t CHAR_4_DATA[] = {
+        _PAD(0b0010),
+        _PAD(0b0110),
+        _PAD(0b1010),
+        _PAD(0b1111),
+        _PAD(0b0010),
+    };
+
+    const PROGMEM uint8_t CHAR_5_DATA[] = {
+        _PAD(0b1111),
+        _PAD(0b1000),
+        _PAD(0b1110),
+        _PAD(0b0001),
+        _PAD(0b1110),
+    };
+
+    const PROGMEM uint8_t CHAR_6_DATA[] = {
+        _PAD(0b0110),
+        _PAD(0b1000),
+        _PAD(0b1110),
+        _PAD(0b1001),
+        _PAD(0b0110),
+    };
+
+    const PROGMEM uint8_t CHAR_7_DATA[] = {
+        _PAD(0b1111),
+        _PAD(0b0001),
+        _PAD(0b0010),
+        _PAD(0b0010),
+        _PAD(0b0010),
+    };
+
+    const PROGMEM uint8_t CHAR_8_DATA[] = {
+        _PAD(0b0110),
+        _PAD(0b1001),
+        _PAD(0b0110),
+        _PAD(0b1001),
+        _PAD(0b0110),
+    };
+
+    const PROGMEM uint8_t CHAR_9_DATA[] = {
+        _PAD(0b0110),
+        _PAD(0b1001),
+        _PAD(0b0111),
+        _PAD(0b0001),
+        _PAD(0b0110),
+    };
+
     const PROGMEM Glyph CHAR_QMARK(CHAR_QMARK_DATA, 4, 5);
     const PROGMEM Glyph CHAR_EXMARK(CHAR_EXMARK_DATA, 1, 5);
     const PROGMEM Glyph CHAR_SPACE(CHAR_SPACE_DATA, 1, 5);
+    const PROGMEM Glyph CHAR_PERCENT(CHAR_PCT_DATA, 5, 5);
     
     const PROGMEM Glyph UPPERCASE_CHARS[] = {
         Glyph(CHAR_A_DATA, 4, 5),
@@ -270,19 +359,37 @@ namespace graphics {
         Glyph(CHAR_Z_DATA, 4, 5),
     };
 
+    const PROGMEM Glyph NUMBER_CHARS[] = {
+        Glyph(CHAR_0_DATA, 4, 5),
+        Glyph(CHAR_1_DATA, 3, 5),
+        Glyph(CHAR_2_DATA, 4, 5),
+        Glyph(CHAR_3_DATA, 4, 5),
+        Glyph(CHAR_4_DATA, 4, 5),
+        Glyph(CHAR_5_DATA, 4, 5),
+        Glyph(CHAR_6_DATA, 4, 5),
+        Glyph(CHAR_7_DATA, 4, 5),
+        Glyph(CHAR_8_DATA, 4, 5),
+        Glyph(CHAR_9_DATA, 4, 5),
+    };
+
     const Glyph& map_char(char c) {
         if (c >= 'A' && c <= 'Z') {
             return UPPERCASE_CHARS[c - 'A'];
         }
-        // Font size is too small for lowercase letters
+        // Lowercase and uppercase letters look the same
         if (c >= 'a' && c <= 'z') {
             return UPPERCASE_CHARS[c - 'a'];
+        }
+        if (c >= '0' && c <= '9') {
+            return NUMBER_CHARS[c - '0'];
         }
         switch (c) {
         case ' ':
             return CHAR_SPACE;
         case '!':
             return CHAR_EXMARK;
+        case '%':
+            return CHAR_PERCENT;
         case '?':
         default:
             return CHAR_QMARK;
